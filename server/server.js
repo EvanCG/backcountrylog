850878200a027bd6c47a8a532e30c1b0350bfa9d
Evan Griffith <evan@evangriffith.net>
2024-03-06 13:23:52 -0800

    Updated .gitignore and added global error handler and production routing to server.

--- server/server.js
+++ server/server.js
@@ -4,8 +4,17 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// uncomment the below for proxy challenge
+// Handle parsing request body
+app.use(express.json());
 
+/* Attempting to change filepath locations if the request is production vs development */
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, '../build')));
+} else {
+  app.use(express.static(path.resolve(__dirname, '../client')));
+}
+
+/* THIS CODE IS TO TEST THAT THE SERVER IS WORKING */
 const leaderList = [
   {name: 'Anna', id: 'a0'},
   {name: 'Ben', id: 'b0'},
@@ -19,13 +28,26 @@ app.get('/api/leaders', (req, res) => {
 
 
 
+app.get('/', (req, res) => {
+  return res.status(200).sendFile(path.join(__dirname, './index.html'));
+});
+
+/* Catch-all route handler */
+app.use((req, res) => res.status(404).send('This page can\'t be found'));
 
 
-app.use(express.static(path.resolve(__dirname, '../build')));
-app.use(express.json());
 
-app.use(({ code, error }, req, res, next) => {
-  res.status(code).json({ error });
+/* Global error hander */
+app.use((err, req, res, next) => {
+  console.log('----> We are in the global error handler <----');
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
 });
 
 module.exports = app.listen(port, () =>
