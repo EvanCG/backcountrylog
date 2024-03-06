d345b67ff4a54571bcca31fb99cae7ec96c572da
Evan Griffith <evan@evangriffith.net>
2024-03-06 12:11:02 -0800

    Validated TestFetch will grab content from server and render it in client.

--- client/components/TestFetch.jsx
+++ client/components/TestFetch.jsx
@@ -1,13 +1,36 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from 'react';
 
 const TestFetch = (props) => {
-  // do some logic
+  const [names, setNames] = useState(null);
+
+  let mapsArray;
+
+  useEffect(() => {
+    console.log('Inside useeffect');
+    fetch('/api/leaders')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('New data is: ', data);
+        setNames(data);
+      });
+  }, []);
+  if(names != null) {
+    mapsArray = names.map((person) => {
+      return (
+        <li>{person.name}</li>
+      )
+    });
+  }
+  
 
   return (
     <div id="testComponent">
       <p>This is the testComponent now named .jsx</p>
+      <ul>
+        {mapsArray}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
 export default TestFetch;
