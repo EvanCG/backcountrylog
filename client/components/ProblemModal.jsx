38295fef50b4a4a545d890e9ae28a2c40a6aeb16
Evan Griffith <evan@evangriffith.net>
2024-03-07 08:50:15 -0800

    Exclusive clickable buttons for various avy conditions

--- client/components/ProblemModal.jsx
+++ client/components/ProblemModal.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProblemSelector from './ProblemSelector';
 import AspectSelector from './AspectSelector';
 import LikelihoodSelector from './LikelihoodSelector';
 
 const ProblemModal = (props) => {
+  
+  const [problem, setProblem] = useState(null);
+  
   /* Shows the modal */
   const openModal = () => {
     const dialog = document.getElementById('problemdialog');
@@ -22,6 +25,10 @@ const ProblemModal = (props) => {
     dialog.close();
   };
 
+  const selectProblem = (problemId) => {
+    setProblem(problemId);
+  }
+
   return (
     <div>
       <button id="addaproblem" onClick={() => openModal()}>
@@ -31,7 +38,7 @@ const ProblemModal = (props) => {
         <div id="dialogcontainer">
           <div id="mainModal">
             <h3>Add Avalanche Problem</h3>
-            <ProblemSelector />
+            <ProblemSelector selectProblem={selectProblem} problem={problem}/>
             <AspectSelector />
             <LikelihoodSelector />
           </div>
--- client/components/ProblemSelector.jsx
+++ client/components/ProblemSelector.jsx
@@ -5,31 +5,67 @@ const ProblemSelector = (props) => {
     <div>
       <h4>Add Avalanche Problem: </h4>
       <div id="problemsContainer">
-        <div className="problemCard" id="dryloose">
+        <div
+          className={"problemcard".concat(props.problem === 'dryloose' ? ' problemselection' : '')}
+          id="dryloose"
+          onClick={() => props.selectProblem('dryloose')}
+        >
           <p>Dry Loose</p>
         </div>
-        <div className="problemCard" id="stormslab">
+        <div
+          className={"problemcard".concat(props.problem === 'stormslab' ? ' problemselection' : '')}
+          id="stormslab"
+          onClick={() => props.selectProblem('stormslab')}
+        >
           <p>Storm Slab</p>
         </div>
-        <div className="problemCard" id="windslab">
+        <div
+          className={"problemcard".concat(props.problem === 'windslab' ? ' problemselection' : '')}
+          id="windslab"
+          onClick={() => props.selectProblem('windslab')}
+        >
           <p>Wind Slab</p>
         </div>
-        <div className="problemCard" id="cornice">
+        <div
+          className={"problemcard".concat(props.problem === 'cornice' ? ' problemselection' : '')}
+          id="cornice"
+          onClick={() => props.selectProblem('cornice')}
+        >
           <p>Cornice</p>
         </div>
-        <div className="problemCard" id="wetloose">
+        <div
+          className={"problemcard".concat(props.problem === 'wetloose' ? ' problemselection' : '')}
+          id="wetloose"
+          onClick={() => props.selectProblem('wetloose')}
+        >
           <p>Wet Loose</p>
         </div>
-        <div className="problemCard" id="wetslab">
+        <div
+          className={"problemcard".concat(props.problem === 'wetslab' ? ' problemselection' : '')}
+          id="wetslab"
+          onClick={() => props.selectProblem('wetslab')}
+        >
           <p>Wet Slab</p>
         </div>
-        <div className="problemCard" id="persistentslab">
+        <div
+          className={"problemcard".concat(props.problem === 'persistentslab' ? ' problemselection' : '')}
+          id="persistentslab"
+          onClick={() => props.selectProblem('persistentslab')}
+        >
           <p>Persistent Slab</p>
         </div>
-        <div className="problemCard" id="deepslab">
+        <div
+          className={"problemcard".concat(props.problem === 'deepslab' ? ' problemselection' : '')}
+          id="deepslab"
+          onClick={() => props.selectProblem('deepslab')}
+        >
           <p>Deep Slab</p>
         </div>
-        <div className="problemCard" id="glide">
+        <div
+          className={"problemcard".concat(props.problem === 'glide' ? ' problemselection' : '')}
+          id="glide"
+          onClick={() => props.selectProblem('glide')}
+        >
           <p>Glide</p>
         </div>
       </div>
